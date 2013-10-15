f8104f402ed4acf8fb232311c5100aaf52159622
Alex Gindin <agindin1@gmail.com>
2013-10-14 22:33:39 -0400

    initial site creation

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Plwsite::Application.config.secret_key_base = 'ae6a0ecf035693c0587a14d62e872dd9f52562dc1bc522fe0211a9aa2a2dbc5ceeeed5fa1897f9f67825dfd069219a6267f3135297a51f69c0efb6b564e4c8b3'
